e984d56c525161ca2599bff66264b148dd7db7d3
OutOfTime <126634930+OutOfTime080102@users.noreply.github.com>
2024-02-28 22:25:22 +0700

    Create 100.py

---
+++ Easy/100.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        # Base case: if both nodes are None, they are equal
+        if not p and not q:
+            return True
+        # If only one of them is None, they are not equal
+        if not p or not q:
+            return False
+        # If values of corresponding nodes are not equal, they are not equal
+        if p.val != q.val:
+            return False
+        
+        # Recursively check the left and right subtrees
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
