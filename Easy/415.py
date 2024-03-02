52c5af0bd7e0dbda71302dfdbf61a4d347f199ed
OutOfTime <126634930+OutOfTime080102@users.noreply.github.com>
2024-03-02 22:37:44 +0700

    Create 415.py

---
+++ Easy/415.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1=int(num1)
+        num2=int(num2)
+        sum=num1+num2
+        sum=str(sum)
+        return sum
